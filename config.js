cd4bb43636ae2746d23f734fb32cb954f23ea4b9
Nurul Abser <nurul.abser@income.com.sg>
2019-06-21 10:10:06 +0800

    config file removed

--- config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    'API_BASE': 'http://localhost:1337',
-    'DEFAULT_LANG': 'en',
-    'S3_BUCKET': 'dristi.app-data',
-    'DATA_FILE': 'hadith.json',
-    'AWS_ACCESS_KEY': 'AKIAWNH3SYBINJTT3V4E',
-    'AWS_SECRET_KEY': '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',
-    'AWS_REGION': 'ap-southeast-1'
-}
