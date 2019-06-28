43fffa0bf6b08bc3a4ef2e339b4b3fcb3c5646e2
Nurul Abser <abser.it@gmail.com>
2019-06-28 15:49:06 +0800

    Data expiry continue

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,5 +1,6 @@
 import AWS from 'aws-sdk/dist/aws-sdk-react-native';
 import {config} from '../../config';
+import {AsyncStorage} from 'react-native'
 
 const aws_config = {
     'accessKeyId': config.AWS_ACCESS_KEY, 
@@ -24,7 +25,6 @@ async function getBooks() {
     }
 }
 
-
 async function getCategory() {
     try {
         const bucketParams = {
@@ -40,30 +40,60 @@ async function getCategory() {
     }
 }
 
-async function getHadithByBookCategory(book=1, category=1) {
+const getHadithByBookCategory = async (book=1, category=1) => {
     try {
-        const key =`${book}-${category}`;
-        if(data = getDataFromStorage(key)) {
-            return data;
+        const key =`${book}-${category}.json`;
+        let hadiths = null;
+        const hadithsInCache = await getDataFromStorage(key);
+        if(hadithsInCache) {
+            if(hadithsInCache.expiry > new Date()) {
+                return hadithsInCache.data
+            }
+            else {
+                // clear the storage
+            }
         }
+        
         const bucketParams = {
             Bucket: config.AWS_S3_BUCKET,
             Key: key
         } 
-        const data = await s3.getObject(bucketParams).promise();
-        console.log(data.Body.toString())
+        data = await s3.getObject(bucketParams).promise();
+        // console.log(data.Body.toString())
         return JSON.parse(data.Body.toString('utf-8'));
     } catch (err) {
         console.log(err);
     }
 }
 
-function getDataFromStorage(key) {
-
+const getDataFromStorage = async (key) => {
+    try {
+        const data = await AsyncStorage.getItem(key);
+        if(data !== null) {
+            return JSON.parse(data);
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.log(err);
+        throw(`There is an error to fetch the key ${key}:  ${err}`);
+    }
 }
 
-function setDataToStorage(key, value) {
+const setDataToStorage = async (key, value) => {
+    try {
+        const data = await AsyncStorage.setItem(key, value);
+    } catch (err) {
+        console.log(err);
+        throw(`There is an error to set the key ${key}:  ${err}`);
+    }
+}
 
+const getExpireDate = (expireInMinutes = config.DATA_EXPIRY_IN_MINUTES) =>{
+    const now = new Date();
+    let expireTime = new Date(now);
+    expireTime.setMinutes(now.getMinutes() + expireInMinutes);
+    return expireTime;
 }
 
 export {getBooks,getCategory, getHadithByBookCategory}
